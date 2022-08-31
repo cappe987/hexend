09f58882d53daf2fc1a812891679f0dfc3577d1b
Casper Andersson <casper.casan@gmail.com>
2022-08-31 13:11:21 +0200

    fix: change length to fill buffer according to warning
    Signed-off-by: Casper Andersson <casper.casan@gmail.com>

--- src/hexend.c
+++ src/hexend.c
@@ -252,7 +252,7 @@ int get_iface_index(char iface[IFNAMSIZ], int sockfd)
         struct ifreq if_idx;
 
         memset(&if_idx, 0, sizeof(struct ifreq));
-        strncpy(if_idx.ifr_name, iface, IFNAMSIZ - 1);
+        strncpy(if_idx.ifr_name, iface, IFNAMSIZ);
 
         if (ioctl(sockfd, SIOCGIFINDEX, &if_idx) < 0) {
 		ERR("No such device: %s\n", iface);
